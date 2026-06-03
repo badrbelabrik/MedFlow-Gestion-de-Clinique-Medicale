8fc77e82c371abf18c1351bff4675be1172de737
badrbelabrik <badr.belabrik@gmail.com>
2026-06-03 15:50:50 +0100

    implement method to get all specialities

--- src/Repositories/DoctorRepository.php
+++ src/Repositories/DoctorRepository.php
@@ -5,6 +5,9 @@ namespace Repositories;
 use Config\Database;
 use Doctor;
 use PDO;
+use PDOException;
+use Speciality;
+use User;
 
 class DoctorRepository
 {
@@ -16,21 +19,40 @@ class DoctorRepository
 
     public function getDoctorByName(string $firstname,string $lastname):?Doctor{
         try{
-            $sql = "SELECT d.id AS doctor_id,u.id AS user_id,u.firstname,u.lastname,sp.id AS speciality_id,sp.name AS speciality_name FROM users u
-                    JOIN doctors d ON d.id_user = u.id
-                    JOIN specialities sp ON sp.id = d.id_speciality
-                    WHERE u.firstname = ? OR u.lastname = ?";
+            $sql = "SELECT u.id AS user_id, u.firstname, u.lastname, u.email, u.phone, u.role,
+                       sp.id AS speciality_id, sp.name AS speciality_name, sp.description AS speciality_desc,
+                        d.is_active,d.id AS id_doctor
+                FROM users u
+                JOIN doctors d ON d.id_user = u.id
+                LEFT JOIN specialities sp ON sp.id = d.id_speciality
+                WHERE (u.firstname LIKE ? OR u.lastname LIKE ?) AND u.role = 'doctor'";
             $stmt = $this->pdo->prepare($sql);
+            $searchTermFirst = '%' . $firstname . '%';
+            $searchTermLast = '%' . $lastname . '%';
             $stmt->execute([
-                $firstname,
-                $lastname
+                $searchTermFirst,
+                $searchTermLast
             ]);
             $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if($result){
-                return new Doctor(
-                    
-                );
-            }
+            $user = new User(
+                $result['firstname'],
+                $result['lastname'],
+                $result['email'],
+                $result['phone'],
+                $result['role'],
+                $result['user_id']
+            );
+            $speciality = new Speciality(
+                $result['speciality_name'],
+                $result['speciality_desc'],
+                $result['speciality_id']
+            );
+            return new Doctor(
+                $user,
+                $speciality,
+                $result['is_active'],
+                $result['id_doctor']
+            );
 
         }catch(PDOException $e){
             echo "Error :".$e->getMessage();
